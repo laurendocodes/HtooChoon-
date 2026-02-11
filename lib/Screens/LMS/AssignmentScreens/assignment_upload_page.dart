235982fde7803e7f90d873dd7212d5b0f0f785f9
Lauren2610 <laurenshinthant@gmail.com>
2026-02-11 23:10:14 +0630

    folder created: assignment screens

--- lib/Screens/LMS/AssignmentScreens/assignment_upload_page.dart
+++ lib/Screens/LMS/AssignmentScreens/assignment_upload_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class AssignmentDetailPage extends StatelessWidget {
-  const AssignmentDetailPage({Key? key}) : super(key: key);
+class AssignmentUploadPage extends StatelessWidget {
+  const AssignmentUploadPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
